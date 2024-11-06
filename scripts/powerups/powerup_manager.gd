4db5b571875113139aeed931a51f752368f74bfc
alanfogel <alanjfogel@gmail.com>
2024-11-06 15:45:15 -0600

    Made spawning powerups work for any screen size.

--- scripts/powerups/powerup_manager.gd
+++ scripts/powerups/powerup_manager.gd
@@ -25,10 +25,11 @@ func _process(delta: float) -> void:
 
 func spawn_powerup() -> void:
 	var power_up_type = randi() % PowerUpType.size()  # Randomly select a powerup type
+	var viewport_rect = get_viewport().get_visible_rect()
 	var powerup = create_powerup(power_up_type)
 	add_child(powerup)
 	powerup.can_move = true
-	powerup.global_position.x = 630
+	powerup.global_position.x = viewport_rect.size.x + 20 # Spawn just off the right side
 	powerup.global_position.y = 225
 	print("Powerup spawned: ", powerup.power_up_type)
 	print("At x: ", powerup.global_position.x, " y: ", powerup.global_position.y)
