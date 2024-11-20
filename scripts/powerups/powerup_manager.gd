56e4765c376c5488009b23bb899b3eda8cae2189
alanfogel <alanjfogel@gmail.com>
2024-11-20 13:47:56 -0600

    Fixed effect_script not set error

--- scripts/powerups/powerup_manager.gd
+++ scripts/powerups/powerup_manager.gd
@@ -53,8 +53,15 @@ func _process(delta: float) -> void:
 
 
 func spawn_powerup() -> void:
-	var power_up_type = randi() % PowerUpType.size()-1  # Randomly select a powerup type
-	
+	var power_up_type = randi() % PowerUpType.size()  # Randomly select a powerup type
+
+	if power_up_type == PowerUpType.ENERGY_PICKUP:
+		# select a different powerup type
+		if randi() % 2 == 0:
+			power_up_type = PowerUpType.INVINCIBILITY
+		else:
+			power_up_type = PowerUpType.SHIELD
+			
 	# Ensure Shield powerup doesn't spawn before the specified time
 	if power_up_type == PowerUpType.SHIELD and game_instance.elapsed_time < shield_spawn_time:
 		# select a different powerup type
@@ -82,7 +89,7 @@ func spawn_energy_pickup() -> void:
 func create_powerup(power_up_type: int) -> Node:
 	if PowerUpScene:
 		var powerup = PowerUpScene.instantiate()
-		
+		print("Powerup type: ", power_up_type)
 		match power_up_type:
 			PowerUpType.INVINCIBILITY:
 				# print("Invincibility powerup created")
